1e9422fb85fade4c80b805138e0c1ca53e7f56aa
DeringSalazar <desalazarsa@est.utn.ac.cr>
2024-10-22 11:26:04 -0600

    Interfaces
    Se agrega una interfaces para view

---
+++ View/IView.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package View;
+
+/**
+ *
+ * @author Student
+ */
+public interface IView {
+    public void insert();
+    public void read();
+    public void readAll();
+    public void update();
+    public void delete();
+    public void display();
+    public void displayAll();
+    public void showMessage();
+    public void showMessageError();
+}
