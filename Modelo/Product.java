145dc89a062f85d8c1450b31c9892163a6d4485a
DeringSalazar <desalazarsa@est.utn.ac.cr>
2024-10-22 09:08:33 -0600

    Class Product
    Se crea la clase producto con atributo, constructor y tostring

--- Modelo/Product.java
+++ Modelo/Product.java
@@ -4,10 +4,47 @@
  */
 package Modelo;
 
-/**
- *
- * @author Student
- */
 public class Product {
+    private int id;
+    private String description;
+    private double price;
+
+    public Product(int id, String description, double price) {
+        this.id = id;
+        this.description = description;
+        this.price = price;
+    }
+
+    public Product(int id) {
+        this(id,"", 0.0);
+    }
+
+    public Product() {
+        this(0);
+    }
     
+    public int getId() {
+        return id;
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" + "id=" + id + ", description=" + description + ", price=" + price + '}';
+    }
 }
--- examen1/Examen1.java
+++ examen1/Examen1.java
@@ -4,17 +4,13 @@
  */
 package examen1;
 
-/**
- *
- * @author Student
- */
 public class Examen1 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
