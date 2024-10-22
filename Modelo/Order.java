9e883733a12919e8a34ce9d6f764678671a23c75
Sebastian Villega Barquero <sebastianbarquerocr@gmail.com>
2024-10-22 09:28:51 -0600

    Se crea Orden
    Se crea orden con sus respectivos datos

--- Modelo/Order.java
+++ Modelo/Order.java
@@ -9,5 +9,60 @@ package Modelo;
  * @author Student
  */
 public class Order {
+    Customer customer;
+    Product product;
+    int cant;
+    boolean status;
+
+    public Order(Customer customer, Product product, int cant, boolean status) {
+        this.customer = customer;
+        this.product = product;
+        this.cant = cant;
+        this.status = status;
+    }
+
+    public Order(Customer customer, Product product, int cant) {
+        this.customer = customer;
+        this.product = product;
+        this.cant = cant;
+        this.status = false;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public int getCant() {
+        return cant;
+    }
+
+    public void setCant(int cant) {
+        this.cant = cant;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+    
+    
     
 }
+        
+
+
